56f3ed7bd430bd9ff805c86c8c2a6924e1f4e5ba
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-19 14:10:59 +0400

    added error handling

--- app.py
+++ app.py
@@ -67,40 +67,43 @@ class SimpleView(HTTPMethodView):
         file = open('data.csv', 'w')
         file.write(request.body.decode())
         file.close()
+        try:
+            train = pd.read_csv('data.csv', header=0, encoding='latin1')
+            train.drop(['id', 'Unnamed: 0', 'index', 'conv_rate'], axis=1, inplace=True)
+            train['bounce_rate'] = train['bounce_rate'].astype(float)
+
+
+            x = train.values
+            x_scaled = preprocessing.MinMaxScaler().fit_transform(x)
+            df = pd.DataFrame(x_scaled, columns=train.columns)
+            train, test = train_test_split(df, test_size = 0.4)
+
+            target = 'with_conversion'
+            IDcol = 'id'
+
+            xgb4 = XGBClassifier(
+                    learning_rate=0.01,
+                    n_estimators=5000,
+                    max_depth=2,
+                    min_child_weight=1,
+                    gamma=0,
+                    subsample=0.85,
+                    colsample_bytree=0.9,
+                    reg_alpha=0.005,
+                    objective='binary:logistic',
+                    nthread=4,
+                    scale_pos_weight=1,
+                    seed=27)
+
+            predictors = [x for x in train.columns if x not in [target, IDcol]]
+                
+            data = modelfit(xgb4, train, test, predictors)
+            #modelfit(xgb4, train, test, predictors)
+            os.remove('data.csv')
+            return response.json(j.dumps(data))
         
-        train = pd.read_csv('data.csv', header=0, encoding='latin1')
-        train.drop(['id', 'Unnamed: 0', 'index', 'conv_rate'], axis=1, inplace=True)
-        train['bounce_rate'] = train['bounce_rate'].astype(float)
-
-
-        x = train.values
-        x_scaled = preprocessing.MinMaxScaler().fit_transform(x)
-        df = pd.DataFrame(x_scaled, columns=train.columns)
-        train, test = train_test_split(df, test_size = 0.4)
-
-        target = 'with_conversion'
-        IDcol = 'id'
-
-        xgb4 = XGBClassifier(
-            learning_rate=0.01,
-            n_estimators=5000,
-            max_depth=2,
-            min_child_weight=1,
-            gamma=0,
-            subsample=0.85,
-            colsample_bytree=0.9,
-            reg_alpha=0.005,
-            objective='binary:logistic',
-            nthread=4,
-            scale_pos_weight=1,
-            seed=27)
-
-        predictors = [x for x in train.columns if x not in [target, IDcol]]
-        
-        data = modelfit(xgb4, train, test, predictors)
-        #modelfit(xgb4, train, test, predictors)
-        os.remove('data.csv')
-        return response.json(j.dumps(data))
+        except:
+            return response.json(j.dumps('Something went wrong'),status=500)
 
 app.add_route(SimpleView.as_view(), '/ppc.html')
 
